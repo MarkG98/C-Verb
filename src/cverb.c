7b70e98d05775b1ba16ee36bc9d345fafbb81474
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-02-29 18:38:42 -0500

    Modified program to read original wav file and write it (unedited) to a new file to check if they sound the same

--- src/cverb.c
+++ src/cverb.c
@@ -12,6 +12,9 @@
 //#define DEBUG
 #define CIRC_BUFF_SAMPLES 6
 
+// Define Coeffecients
+//#define
+
 //  WAVE file header struct
 typedef struct
 {
@@ -49,22 +52,36 @@ waveheader header;
 cbuf_handle_t inputBuff;
 processingBuffer pBuff;
 
+
 // Parses the wav file and fills a waveheader struct with the resulting information
 // The code in this function was taken from http://truelogic.org/wordpress/2015/09/04/parsing-a-wav-file-in-c/
-void parse_wav (FILE *in_file)
+/* Function which reads through a wav file header
+
+	 Reads wav file header and populates waveheader struct
+	 and writes the header to a new wave file which will
+	 contain the filtered signal.
+
+	 in_file: wav file to be filtered
+	 out_file: wav file to contain filtered data
+*/
+void parse_wav (FILE *in_file, FILE *out_file)
 {
 	 int read = 0;
+	 int write = 0;
 
 	 // read header parts
 	 unsigned char buffer4[4];
 	 unsigned char buffer2[2];
 
 	 read = fread(header.riff, sizeof(header.riff), 1, in_file);
+	 write = fwrite(header.riff, sizeof(header.riff), 1, out_file);
+
 	 #ifdef DEBUG
 	 printf("(1-4): %s \n", header.riff);
 	 #endif
 
 	 read = fread(buffer4, sizeof(buffer4), 1, in_file);
+	 write = fwrite(buffer4, sizeof(buffer4), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	 #endif
@@ -80,16 +97,19 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(header.wave, sizeof(header.wave), 1, in_file);
+	 write = fwrite(header.wave, sizeof(header.wave), 1, out_file);
 	 #ifdef DEBUG
 	 printf("(9-12) Wave marker: %s\n", header.wave);
 	 #endif
 
 	 read = fread(header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), 1, in_file);
+	 write = fwrite(header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), 1, out_file);
 	 #ifdef DEBUG
 	 printf("(13-16) Fmt marker: %s\n", header.fmt_chunk_marker);
 	 #endif
 
 	 read = fread(buffer4, sizeof(buffer4), 1, in_file);
+	 write = fwrite(buffer4, sizeof(buffer4), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	 #endif
@@ -103,6 +123,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer2, sizeof(buffer2), 1, in_file);
+	 write = fwrite(buffer2, sizeof(buffer2), 1, out_file);
 
 	 #ifdef DEBUG
 	 printf("%u %u \n", buffer2[0], buffer2[1]);
@@ -122,6 +143,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer2, sizeof(buffer2), 1, in_file);
+	 write = fwrite(buffer2, sizeof(buffer2), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u \n", buffer2[0], buffer2[1]);
 	 #endif
@@ -132,6 +154,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer4, sizeof(buffer4), 1, in_file);
+	 write = fwrite(buffer4, sizeof(buffer4), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	 #endif
@@ -146,6 +169,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer4, sizeof(buffer4), 1, in_file);
+	 write = fwrite(buffer4, sizeof(buffer4), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	 #endif
@@ -159,6 +183,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer2, sizeof(buffer2), 1, in_file);
+	 write = fwrite(buffer2, sizeof(buffer2), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u \n", buffer2[0], buffer2[1]);
 	 #endif
@@ -170,6 +195,7 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(buffer2, sizeof(buffer2), 1, in_file);
+	 write = fwrite(buffer2, sizeof(buffer2), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u \n", buffer2[0], buffer2[1]);
 	 #endif
@@ -181,11 +207,13 @@ void parse_wav (FILE *in_file)
 	 #endif
 
 	 read = fread(header.data_chunk_header, sizeof(header.data_chunk_header), 1, in_file);
+	 write = fwrite(header.data_chunk_header, sizeof(header.data_chunk_header), 1, out_file);
 	 #ifdef DEBUG
 	 printf("(37-40) Data Marker: %s \n", header.data_chunk_header);
 	 #endif
 
 	 read = fread(buffer4, sizeof(buffer4), 1, in_file);
+	 write = fwrite(buffer4, sizeof(buffer4), 1, out_file);
 	 #ifdef DEBUG
 	 printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	 #endif
@@ -200,34 +228,35 @@ void parse_wav (FILE *in_file)
 	 #endif
 }
 
-void process_data (FILE *out_file)
-{
-
-}
-
 void retrieve_sample (int16_t *retrieved)
 {
 		circular_buf_get(inputBuff, ((char*) retrieved));
 		circular_buf_get(inputBuff, ((char*) retrieved)+1);
 }
 
-void buffer_sample(FILE *in_file)
+void buffer_sample(FILE *in_file, FILE *out_file)
 {
 		int read;
+		int write;
 
 		read = fread(&sample_union.intermediate_sample, sizeof(sample_union.intermediate), 1, in_file);
 
-		sample_union.intermediate_sample = (sample_union.intermediate[0])|(sample_union.intermediate[1]<<8);
+		sample_union.intermediate_sample = (sample_union.intermediate_sample << 8) | ((sample_union.intermediate_sample >> 8) & 0xFF);
 
 		circular_buf_put(inputBuff, sample_union.intermediate[0]);
 		circular_buf_put(inputBuff, sample_union.intermediate[1]);
 }
 
-void load_data (FILE *in_file)
+void load_data (FILE *in_file, FILE*out_file)
 {
 		int16_t retrieved;
 
+		buffer_sample(in_file, out_file);
 		retrieve_sample(&retrieved);
+
+		sample_union.intermediate_sample = retrieved;
+		sample_union.intermediate_sample = (sample_union.intermediate_sample << 8) | ((sample_union.intermediate_sample >> 8) & 0xFF);
+		fwrite(&sample_union.intermediate_sample, sizeof(sample_union.intermediate_sample), 1, out_file);
 
 		pBuff.buffer[pBuff.head] = retrieved;
 		if (pBuff.head < CIRC_BUFF_SAMPLES)
@@ -257,8 +286,9 @@ int main (int argc, char *argv[])
     argv += optind;
 
     FILE *in_file = fopen(argv[0], "rb");
+		FILE *out_file = fopen("C-Verb.wav", "w");
 
-		parse_wav(in_file);
+		parse_wav(in_file, out_file);
 
 		uint8_t buffOnStack[CIRC_BUFF_SAMPLES * (header.bits_per_sample / 8)];
 		inputBuff = circular_buf_init(buffOnStack, CIRC_BUFF_SAMPLES * (header.bits_per_sample / 8));
@@ -270,8 +300,8 @@ int main (int argc, char *argv[])
 		}
 		pBuff.head = 0;
 
-
-		buffer_sample(in_file);
-		load_data(in_file);
-
+		while (!feof(in_file))
+		{
+			load_data(in_file, out_file);
+		}
 }
