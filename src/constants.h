fb6c57d1a03aa7fa1509ad1872cfeb6e55626028
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-03-25 18:43:24 -0400

    Added seperate feedback constants for comb and all pass filters

--- src/constants.h
+++ src/constants.h
@@ -2,7 +2,9 @@
 #define CIRC_BUFF_SAMPLES 6 // Circular buffer capacity
 #define CIRC_BUFF_SIZE CIRC_BUFF_SAMPLES * (header->bits_per_sample / 16) // Calculate size of circular buffer
 #define DELAY 32 // Length of delay [ms]
-#define FF 1.0 // Feedfoward gain
-#define FB 0.8 // Feedback gain
+#define FF_C 1.0 // Feedfoward gain comb filter
+#define FB_C 0.2 // Feedback gain comb filter
+#define FF_A 1.0 // Feedfoward gain comb filter
+#define FB_A 0.12 // Feedback gain comb filter
 #define PBUFF_LENGTH ((5 * DELAY * (header->sample_rate) / 1000) + 50) // Calculate length of proecessing buffer
 #define DELAY_SAMPLES  DELAY * header->sample_rate / 1000 // Calculate amount of samples for one delay length
